fa291891498103651d864263f9124236ed388b4f
truong quoc tuan <truongquoctuan1409@gmail.com>
2022-09-27 16:13:23 +0700

    update customize-cra prettier

--- src/App.js
+++ src/App.js
@@ -1,32 +1,32 @@
-import { Route, Routes, Link } from "react-router-dom";
+import { Route, Routes, Link } from 'react-router-dom';
 import './App.css';
-import HomePage from './pages/Home'
-import ContactPage from './pages/Contact'
-import NewsPage from './pages/News'
+import HomePage from './pages/Home';
+import ContactPage from './pages/Contact';
+import NewsPage from './pages/News';
 
 function App() {
-  return (
-    <div className="App">
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact</Link>
-          </li>
-          <li>
-            <Link to="/news">News</Link>
-          </li>
-        </ul>
-      </nav>
-      <Routes>
-        <Route path="/" element={<HomePage/>}/>
-        <Route path="/contact" element={<ContactPage/>}/>
-        <Route path="/news" element={<NewsPage/>}/>
-      </Routes>
-    </div>
-  );
+    return (
+        <div className="App">
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/contact">Contact</Link>
+                    </li>
+                    <li>
+                        <Link to="/news">News</Link>
+                    </li>
+                </ul>
+            </nav>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/contact" element={<ContactPage />} />
+                <Route path="/news" element={<NewsPage />} />
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
