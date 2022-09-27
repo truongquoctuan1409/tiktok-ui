7e3747578dd39019278d8e65446a0607a3ab8a17
truong quoc tuan <truongquoctuan1409@gmail.com>
2022-09-27 15:58:00 +0700

    update customize-cra

---
+++ src/pages/Contact.js
@@ -0,0 +1,10 @@
+import logo from '../logo.svg';
+
+export default function Contact(){
+    return (
+        <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1>Contact</h1>
+        </header>
+    )
+}
---
+++ src/pages/News.js
@@ -0,0 +1,10 @@
+import logo from '../logo.svg';
+
+export default function News(){
+    return (
+        <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1>News</h1>
+        </header>
+    )
+}
