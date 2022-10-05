6c65c1cbe24b033e424ee04791cb503dc9fbf86f
truong quoc tuan <truongquoctuan1409@gmail.com>
2022-10-05 17:51:47 +0700

    fix used axios in apiService/searchService.js

--- src/apiService/searchService.js
+++ src/apiService/searchService.js
@@ -2,7 +2,7 @@ import * as request from '../utils/request';
 
 export const search = async (q, type = 'less') => {
     try {
-        const res = await request.get(`https://tiktok.fullstack.edu.vn/api/users/search`, {
+        const res = await request.get(`users/search`, {
             params: {
                 q,
                 type,
