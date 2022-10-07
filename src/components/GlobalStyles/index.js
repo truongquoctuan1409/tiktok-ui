960b193b1fb7f68bf4132179e6549b408c9bf31f
truong quoc tuan <truongquoctuan1409@gmail.com>
2022-10-07 21:53:27 +0700

    handle more file index.js

--- src/components/GlobalStyles/index.js
+++ src/components/GlobalStyles/index.js
@@ -1,9 +1,1 @@
-import PropTypes from 'prop-types';
-import './GlobalStyles.scss';
-
-export default function GlobalStyles({ children }) {
-    return children;
-}
-GlobalStyles.propTypes = {
-    children: PropTypes.node.isRequired,
-};
+export { default } from './GlobalStyles';
---
+++ src/layouts/components/Header/Header.js
@@ -0,0 +1,164 @@
+import classNames from 'classnames/bind';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+    faPlus,
+    faEllipsisVertical,
+    faEarthAsia,
+    faCircleQuestion,
+    faKeyboard,
+    faCloudArrowUp,
+    faGear,
+    faCoins,
+    faArrowRightFromBracket,
+} from '@fortawesome/free-solid-svg-icons';
+import { faPaperPlane, faUser, faComments } from '@fortawesome/free-regular-svg-icons';
+import Tippy from '@tippyjs/react';
+import 'tippy.js/dist/tippy.css';
+import { Link } from 'react-router-dom';
+
+import styles from './Header.module.scss';
+import images from '~/assets/images';
+import Button from '~/components/Button';
+import Menu from '~/components/Popper/Menu';
+import Image from '~/components/Image';
+// import { MailboxIcon } from '../../../../components/Icons';
+import Search from '../Search';
+import config from '~/config';
+
+const cx = classNames.bind(styles);
+const MENU_ITEMS = [
+    {
+        icon: <FontAwesomeIcon icon={faEarthAsia} />,
+        title: 'English',
+        children: {
+            title: 'Language',
+            data: [
+                {
+                    type: 'language',
+                    code: 'en',
+                    title: 'English',
+                },
+                {
+                    type: 'language',
+                    code: 'vi',
+                    title: 'Tiếng Việt',
+                },
+                {
+                    type: 'language',
+                    code: 'china',
+                    title: 'Shennana',
+                },
+                {
+                    type: 'language',
+                    code: 'japan',
+                    title: 'Mikami fukada',
+                },
+            ],
+        },
+    },
+    {
+        icon: <FontAwesomeIcon icon={faCircleQuestion} />,
+        title: 'Feedback and help',
+        to: '/feedback',
+    },
+    {
+        icon: <FontAwesomeIcon icon={faKeyboard} />,
+        title: 'Keyboard shortcuts',
+    },
+];
+const userMenu = [
+    {
+        icon: <FontAwesomeIcon icon={faUser} />,
+        title: 'View profile',
+        to: '/@tuan',
+    },
+    {
+        icon: <FontAwesomeIcon icon={faCoins} />,
+        title: 'Get coins',
+        to: '/coin',
+    },
+    {
+        icon: <FontAwesomeIcon icon={faGear} />,
+        title: 'Setting',
+        to: '/setting',
+    },
+    ...MENU_ITEMS,
+    {
+        icon: <FontAwesomeIcon icon={faArrowRightFromBracket} />,
+        title: 'Log out',
+        to: '/logout',
+        separate: true,
+    },
+];
+const boxMessage = [1, 2, 3];
+
+function Header() {
+    const currentUser = true;
+    const handleMenuItems = (item) => {
+        switch (item.type) {
+            case 'language':
+                console.log(item);
+                break;
+            default:
+        }
+    };
+
+    return (
+        <header className={cx('wrapper')}>
+            <div className={cx('inner')}>
+                <Link to={config.routes.home} className={cx('logo-link')}>
+                    <img src={images.logo} alt="tiktok" />
+                </Link>
+                <Search />
+                <div className={cx('actions')}>
+                    {currentUser ? (
+                        <>
+                            <Tippy delay={[0, 150]} content="Upload video" placement="bottom">
+                                <button className={cx('action-btn')}>
+                                    <FontAwesomeIcon icon={faCloudArrowUp} />
+                                </button>
+                            </Tippy>
+                            <Tippy delay={[0, 150]} content="Message" placement="bottom">
+                                <button className={cx('action-btn')}>
+                                    <FontAwesomeIcon icon={faPaperPlane} />
+                                </button>
+                            </Tippy>
+                            <Tippy delay={[0, 150]} content="Mailbox" placement="bottom">
+                                <button className={cx('action-btn')}>
+                                    {boxMessage.length > 0 ? (
+                                        <span className={cx('mess')}>{boxMessage.length}</span>
+                                    ) : (
+                                        ''
+                                    )}
+                                    <FontAwesomeIcon icon={faComments} />
+                                </button>
+                            </Tippy>
+                        </>
+                    ) : (
+                        <>
+                            <Button text leftIcon={<FontAwesomeIcon icon={faPlus} />}>
+                                Upload
+                            </Button>
+                            <Button primary>Login</Button>
+                        </>
+                    )}
+                    <Menu items={currentUser ? userMenu : MENU_ITEMS} onChange={handleMenuItems}>
+                        {currentUser ? (
+                            <Image
+                                className={cx('user-avatar')}
+                                src="https://taimienphi.vn/tmp/cf/aut/anh-gai-xinh-1.jpg"
+                                alt="Nguyen Van A"
+                                fallback="https://haycafe.vn/wp-content/uploads/2022/02/Anh-gai-xinh-Viet-Nam.jpg"
+                            />
+                        ) : (
+                            <button className={cx('more-btn')}>
+                                <FontAwesomeIcon icon={faEllipsisVertical} />
+                            </button>
+                        )}
+                    </Menu>
+                </div>
+            </div>
+        </header>
+    );
+}
+export default Header;
