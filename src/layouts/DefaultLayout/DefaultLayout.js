f27359b657397a32e68781bb5ebeb0441d4d0e2f
truong quoc tuan <truongquoctuan1409@gmail.com>
2022-10-11 20:30:52 +0700

    UI sidebar #1

--- src/layouts/DefaultLayout/DefaultLayout.js
+++ src/layouts/DefaultLayout/DefaultLayout.js
@@ -1,9 +1,9 @@
-import styles from './DefaultLayout.module.scss';
 import classNames from 'classnames/bind';
 import PropTypes from 'prop-types';
 
+import styles from './DefaultLayout.module.scss';
 import Header from '~/layouts/components/Header';
-import Sidebar from './Sidebar';
+import Sidebar from '~/layouts/components/Sidebar';
 
 const cx = classNames.bind(styles);
 
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -6,12 +6,14 @@ import Following from '~/pages/Following';
 import Upload from '~/pages/Upload';
 import Search from '~/pages/Search';
 import Profile from '~/pages/Profile';
+import Live from '~/pages/Live';
 import config from '~/config';
 
 //public routes
 const publicRoutes = [
     { path: config.routes.home, component: Home },
     { path: config.routes.following, component: Following },
+    { path: config.routes.live, component: Live },
     { path: config.routes.profile, component: Profile },
     { path: config.routes.upload, component: Upload, layout: HeaderOnly },
     { path: config.routes.search, component: Search, layout: null },
